bb03af0be3ce15b798858a3e9454ad46e15b69a8
ldhmtx <ldhmtx@umsystem.edu>
2024-04-03 13:33:29 -0500

    Update CountingSort.py

--- pythonProject/src/CountingSort.py
+++ pythonProject/src/CountingSort.py
@@ -1,17 +1,31 @@
 # Luke Hill's Counting Sort Algorithm
 
-def findmax(arr: list) -> list:
+# pre: take in list
+# post: return max of list
+def findmax(arr: list) -> int:
     maximum = 0
     for i in range(len(arr)):
         if arr[i] > maximum:
             maximum = arr[i]
     return maximum
 
+# pre: take in an integer
+# post: return empty list of size integer
 def initializelist(ind: int) -> list:
     fill = 0
     arr = []
-    for i in range(ind):
+    for i in range(ind+1):
         arr.append(fill)
     return arr
 
-print(findmax([1, 4 , 6 ,9]))
+# pre: take in unsorted list
+# post: return
+def countsort(arr: list) -> list:
+    m = findmax(arr)
+    countArray = initializelist(arr)
+    # count the number of occurences of each number at respective index
+
+    # temp return
+    return arr
+
+print(countsort([1, 4 , 6 ,9]))
