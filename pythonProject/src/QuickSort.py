143ef0bfc79c5d6ba67fc3dc0eb5d16b444a8865
Vulpolox <113536413+Vulpolox@users.noreply.github.com>
2024-04-08 12:57:45 -0500

    Finished implementing quick sort

--- pythonProject/src/QuickSort.py
+++ pythonProject/src/QuickSort.py
@@ -1,28 +1,36 @@
 # written by Jack Keys
 
 import statistics
+import copy
+import MeasureExectution
+
 
 # pre  -- takes a list
 # post -- sorts and returns the list
-def quickSort(lst: list) -> list:
+def quick_sort(lst: list) -> list:
 
-    if len(lst <= 1):        # base case: list has less than 2 elements in it
+    if len(lst) <= 1:                  # base case: list has less than 2 elements in it
         return lst
 
-    pivot = selectPivot(lst) # select a pivot
-    lstNoPivot = lst.remove(pivot)
-    left = []
-    right = []
+    pivot = select_pivot(lst)          # select a pivot
+    lst_no_pivot = copy.deepcopy(lst)  # create deep copy of lst and set it to lst_no_pivot
+    lst_no_pivot.remove(pivot)         # remove pivot from lst_no_pivot
+    left = []                          # list for holding values less than pivot
+    right = []                         # list for holding values greater than pivot
 
-    for number in lstNoPivot:
-        pass
+    for number in lst_no_pivot:
 
+        if number <= pivot:            # if number is less than the pivot
+            left.append(number)
+        else:                          # if number is greater than the pivot
+            right.append(number)
 
+    return quick_sort(left) + [pivot] + quick_sort(right)  # recursive call
 
 
 # pre  -- takes a list
 # post -- returns the median between the first, the last,
 #         and the middle element of the list (median of three)
-def selectPivot(lst: list) -> int:
-    possiblePivots = [lst[1], lst[len(lst) // 2], lst[-1]]
-    return statistics.median(possiblePivots)
+def select_pivot(lst: list) -> int:
+    possible_pivots = [lst[1], lst[len(lst) // 2], lst[-1]]
+    return statistics.median(possible_pivots)
