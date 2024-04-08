b72e220c6dcff4557f66342474e8db23c17914c8
ldhmtx <ldhmtx@umsystem.edu>
2024-04-08 12:36:19 -0500

    Update CountingSort.py

--- pythonProject/src/CountingSort.py
+++ pythonProject/src/CountingSort.py
@@ -1,12 +1,12 @@
 # Luke Hill's Counting Sort Algorithm
-import measureExecution
+import MeasureExectution
 import time
 
 # counting sort functions [call countsort(list) for sorted]:
 # ------------------------------------------------------------
 # pre: take in list
 # post: return max of list
-def findmax(lst: list) -> int:
+def find_max(lst: list) -> int:
     maximum = 0
 
     # for size of list, if larger find max
@@ -18,7 +18,7 @@ def findmax(lst: list) -> int:
 
 # pre: take in an integer
 # post: return empty list of size integer
-def initializelist(ind: int) -> list:
+def initialize_list(ind: int) -> list:
     lst = []
 
     # for given size (max in count sort), create lst of proper size
@@ -29,27 +29,27 @@ def initializelist(ind: int) -> list:
 
 # pre: take in unsorted list
 # post: return sorted list
-def countsort(lst: list) -> list:
-    m = findmax(lst)  # max element in list
-    countlst = initializelist(m)  # create lst of size max+1, filled with zeroes
+def count_sort(lst: list) -> list:
+    m = find_max(lst)  # max element in list
+    count_lst = initialize_list(m)  # create lst of size max+1, filled with zeroes
 
     # add 1 to every corresponding index to a number
     for num in lst:
-        countlst[num] += 1
+        count_lst[num] += 1
 
     # from 1 to max+1, update cumulative sum across all elements (last will be size of original list)
     for i in range(1, m + 1):
-        countlst[i] += countlst[i - 1]
+        count_lst[i] += count_lst[i - 1]
 
-    sortedlst = [0] * len(lst)  # initialize output list
+    sorted_lst = [0] * len(lst)  # initialize output list
 
     # for the elements of original list, use cumulative count to place elements into sorted list in the correct spot
     # countlst is decremented to keep track of what we've placed so far
     for num in lst:
-        sortedlst[countlst[num] - 1] = num
-        countlst[num] -= 1
+        sorted_lst[count_lst[num] - 1] = num
+        count_lst[num] -= 1
 
-    return sortedlst
+    return sorted_lst
 
-measureexecution(countsort)
+MeasureExectution.measureexecution(count_sort)
 
