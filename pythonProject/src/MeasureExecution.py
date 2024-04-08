7a240eff32b8a4a03b32ef7b8aba626077ecb795
ldhmtx <ldhmtx@umsystem.edu>
2024-04-08 12:50:01 -0500

    Update MeasureExecution.py

--- pythonProject/src/MeasureExecution.py
+++ pythonProject/src/MeasureExecution.py
@@ -3,10 +3,12 @@ import time
 
 #
 #
-def measureexecution(func):
+
+
+def measure_execution(func):
     # for number of times we want to measure:
     num_times = 100
-    exectimes_lst = []
+    exec_times_lst = []
 
     # running the sort algorithm for numtimes
     for i in range(num_times):
@@ -17,22 +19,22 @@ def measureexecution(func):
         func(ListGenerator.randList())
         end = time.time()  # record time ending
         lst.append(end - begin)
-        exectimes_lst.append(lst)  # add lst with two values (size, time) for each
+        exec_times_lst.append(lst)  # add lst with two values (size, time) for each
 
     avg = 0
     avg_size = 0
     kor = False  # set true if want individual information of each size/runtime
 
     # display individual results if turned on using bool kor, otherwise calculates average time and size
-    for i in exectimes_lst:
+    for i in exec_times_lst:
         if kor:
             print(f"Input size: {i[0]} | Execution time: {i[1]}s\n")
         avg += i[1]
         avg_size += i[0]
 
     # find best/worst cases
-    max_time = max(exectimes_lst, key=lambda x: x[1])
-    min_time = min(exectimes_lst, key=lambda x: x[1])
+    max_time = max(exec_times_lst, key=lambda x: x[1])
+    min_time = min(exec_times_lst, key=lambda x: x[1])
 
     # display ultimate results
     print("\n")
